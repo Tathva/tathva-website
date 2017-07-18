db88269c96bb57c71a388036b66b711590d80393
Chris <christopher.johns@tathva.ed.jp>
2017-07-18 16:26:20 +0900

    Added banner image parallax to homepage, updated content and rules.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { SwiperDirective, SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { Component } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css', './home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
 
   public config: SwiperConfigInterface = {
@@ -18,11 +18,6 @@ export class HomeComponent implements OnInit {
     autoplay: 5000,
     autoplayDisableOnInteraction: false
   };
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   onIndexChange(index: number) {
     console.log('Swiper index: ' + index);
