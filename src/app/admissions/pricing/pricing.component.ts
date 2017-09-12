9c530984b8e02eceb464aa391e3557d3367c7ae0
Chris <christopher.johns@tathva.ed.jp>
2017-09-12 10:47:57 +0900

    Pricing & Fees updated

--- src/app/admissions/pricing/pricing.component.ts
+++ src/app/admissions/pricing/pricing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.scss', '../.././shared/module-content.scss', './material.min.css']
+  styleUrls: ['./pricing.component.scss', '../.././shared/module-content.scss', './material_table.scss']
 })
 export class PricingComponent implements OnInit {
 
