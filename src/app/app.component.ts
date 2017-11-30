36bc3da510b50dd0821265a9eb4f8db9c0cbc812
Chris <christopher.johns@tathva.ed.jp>
2017-11-30 16:19:27 +0900

    Window scroll on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Location, PopStateEvent } from '@angular/common';
 
 interface Link {
   url: string,
@@ -15,7 +17,7 @@ interface LinkGroup {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   nav_groups: LinkGroup[] = [
     {
@@ -101,11 +103,34 @@ export class AppComponent {
     }
   ];
 
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
+
   public isOpen = false;
   public menuIcon = 'menu';
 
-  constructor() {
+  constructor(private router: Router, private location: Location) {
   };
+
+  ngOnInit() {
+    this.location.subscribe((ev: PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+    this.router.events.subscribe((ev: any) => {
+      if (ev instanceof NavigationStart) {
+        if (ev.url !== this.lastPoppedUrl) {
+          this.yScrollStack.push(window.scrollY);
+        }
+      } else if (ev instanceof NavigationEnd) {
+        if (ev.url === this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        } else {
+          window.scrollTo(0, 0);
+        }
+      }
+    });
+  }
 
   public onOpen() {
     this.isOpen = true;
